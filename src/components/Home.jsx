930279a14bbd84883f264475ef4b59dab108092e
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-26 12:35:49 -0700

    Began building the web page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import SortingGraph from "./SortingGraph";
 
 function Home() {
+  const [activeAlgorithm, setActiveAlgorithm] = useState("Quick Sort");
+
+  const handleSortChange = (e) => {
+    e.preventDefault();
+    setActiveAlgorithm(e.target.value);
+  };
+
   return (
     <div>
-      <h1>Sort it Out</h1>
+      <header>
+        <h1>{activeAlgorithm}</h1>
+        <button
+          type="button"
+          className="change-sort-button"
+          value="Quick Sort"
+          onClick={handleSortChange}
+        >
+          Quick Sort
+        </button>
+        <button
+          className="change-sort-button"
+          value="Merge Sort"
+          onClick={handleSortChange}
+        >
+          Merge Sort
+        </button>
+        <button
+          className="change-sort-button"
+          value="Bubble Sort"
+          onClick={handleSortChange}
+        >
+          Bubble Sort
+        </button>
+      </header>
+      <div>
+        <SortingGraph algorithm={activeAlgorithm} />
+      </div>
     </div>
   );
 }
---
+++ src/components/SortingGraph.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+function SortingGraph({ algorithm }) {
+  return <div className="sorting-graph"></div>;
+}
+
+export default SortingGraph;
