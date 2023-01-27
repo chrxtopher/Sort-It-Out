99993c8320bd656eeb18f4e79ac0666a29c27f52
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-26 22:05:12 -0700

    Implemented New Array button & width determination for graph bars
    Also changed min value for an item in the array to be 10

--- src/components/SortingGraph.jsx
+++ src/components/SortingGraph.jsx
@@ -3,42 +3,72 @@ import GraphBar from "./GraphBar";
 
 function SortingGraph({ algorithm }) {
   const [numberArray, setNumberArray] = useState(
-    new Array(800)
+    new Array(150)
       .fill(0)
-      .map((number) => Math.floor(Math.random() * (650 - 5) + 5))
+      .map((number) => Math.floor(Math.random() * (650 - 10) + 10))
   );
-
-  const bubbleSort = async (array) => {
-    for (let i = 0; i < array.length; i++) {
-      for (let j = 0; j < array.length - i - 1; j++) {
-        await pause(1);
-        if (array[j + 1]) {
-          if (array[j] > array[j + 1]) {
-            const temp = array[j];
-            array[j] = array[j + 1];
-            array[j + 1] = temp;
-            setNumberArray(numberArray.map((number, index) => array[index]));
-          }
-        }
-      }
-    }
-  };
 
   const pause = (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  const handleNewArrayClick = () => {
+    const newArray = new Array(numberArray.length)
+      .fill(0)
+      .map((number) => Math.floor(Math.random() * (650 - 10) + 10));
+    setNumberArray(numberArray.map((number, index) => newArray[index]));
   };
 
   const handleSortClick = () => {
     bubbleSort(numberArray);
   };
 
+  const determineWidth = (total) => {
+    if (total >= 850) {
+      return 1;
+    } else if (total >= 625) {
+      return 2;
+    } else if (total >= 450) {
+      return 3;
+    } else if (total >= 300) {
+      return 4;
+    } else if (total >= 175) {
+      return 5;
+    } else {
+      return 10;
+    }
+  };
+
+  /////////////////
+  // Bubble Sort //
+  /////////////////
+
+  const bubbleSort = async (array) => {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array.length - i - 1; j++) {
+        await pause();
+        if (array[j] > array[j + 1]) {
+          const temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+          setNumberArray(numberArray.map((number, index) => array[index]));
+        }
+      }
+    }
+  };
+
   return (
     <div className="sorting-graph">
-      <h1>Sorting with {algorithm}</h1>
+      <h1>{algorithm}</h1>
+      <button onClick={handleNewArrayClick}>New Array</button>
       <button onClick={handleSortClick}>Sort</button>
       <div className="graph">
         {numberArray.map((number) => (
-          <GraphBar value={number} color="pink" />
+          <GraphBar
+            value={number}
+            color="pink"
+            width={determineWidth(numberArray.length)}
+          />
         ))}
       </div>
     </div>
