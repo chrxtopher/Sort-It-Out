fbf9901be051bc2c6130fd3b539b6daee49ea007
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-26 20:07:27 -0700

    Removed setTimeout block with a pause function
    Also adjusted number of items being sorted. This will be changed later in state

--- src/components/SortingGraph.jsx
+++ src/components/SortingGraph.jsx
@@ -3,28 +3,29 @@ import GraphBar from "./GraphBar";
 
 function SortingGraph({ algorithm }) {
   const [numberArray, setNumberArray] = useState(
-    new Array(300)
+    new Array(800)
       .fill(0)
-      .map((number) => Math.floor(Math.random() * (750 - 5) + 5))
+      .map((number) => Math.floor(Math.random() * (650 - 5) + 5))
   );
 
-  const bubbleSort = (array) => {
+  const bubbleSort = async (array) => {
     for (let i = 0; i < array.length; i++) {
       for (let j = 0; j < array.length - i - 1; j++) {
-        setTimeout(() => {
-          if (array[j + 1]) {
-            if (array[j] > array[j + 1]) {
-              const temp = array[j];
-
-              array[j] = array[j + 1];
-              array[j + 1] = temp;
-
-              setNumberArray(numberArray.map((number, index) => array[index]));
-            }
+        await pause(1);
+        if (array[j + 1]) {
+          if (array[j] > array[j + 1]) {
+            const temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+            setNumberArray(numberArray.map((number, index) => array[index]));
           }
-        }, 500);
+        }
       }
     }
+  };
+
+  const pause = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   };
 
   const handleSortClick = () => {
