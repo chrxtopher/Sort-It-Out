0e79d4badfb9b8847cc8f5f96f6618faa7f8567a
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-27 20:45:21 -0700

    Changed array of numbers to an array of objects, added state and form that controls speed
    each object has a value and active key that will be used to control some css.
    
    the two elements being sorted in bubble sort are now highlighted in blue. (easier to visualize how the sorting is working)

--- src/components/SortingGraph.jsx
+++ src/components/SortingGraph.jsx
@@ -5,10 +5,13 @@ function SortingGraph() {
   const [activeAlgorithm, setActiveAlgorithm] = useState("Bubble Sort");
   const [message, setMessage] = useState("Ready");
   const [numberArray, setNumberArray] = useState(
-    new Array(150)
-      .fill(0)
-      .map((number) => Math.floor(Math.random() * (650 - 10) + 10))
+    new Array(150).fill({}).map((item) => ({
+      active: false,
+      value: Math.floor(Math.random() * (650 - 10) + 10),
+    }))
   );
+
+  const [speed, setSpeed] = useState(5);
 
   const pause = (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -20,15 +23,20 @@ function SortingGraph() {
   };
 
   const handleNewArrayClick = () => {
-    const newArray = new Array(numberArray.length)
-      .fill(0)
-      .map((number) => Math.floor(Math.random() * (650 - 10) + 10));
+    const newArray = new Array(150).fill({}).map((item) => ({
+      active: false,
+      value: Math.floor(Math.random() * (650 - 10) + 10),
+    }));
     setNumberArray(numberArray.map((number, index) => newArray[index]));
     setMessage("Ready");
   };
 
   const handleSortClick = () => {
     bubbleSort(numberArray);
+  };
+
+  const handleSpeedChange = (e) => {
+    setSpeed(e.target.value);
   };
 
   const determineWidth = (total) => {
@@ -56,13 +64,19 @@ function SortingGraph() {
     for (let i = 0; i < array.length; i++) {
       if (i === array.length - 1) setMessage("Sorted");
       for (let j = 0; j < array.length - i - 1; j++) {
-        await pause();
-        if (array[j] > array[j + 1]) {
-          const temp = array[j];
-          array[j] = array[j + 1];
-          array[j + 1] = temp;
-          setNumberArray(numberArray.map((number, index) => array[index]));
+        array[j].active = true;
+        array[j + 1].active = true;
+        await pause(speed);
+
+        if (array[j].value > array[j + 1].value) {
+          const temp = array[j].value;
+          array[j].value = array[j + 1].value;
+          array[j + 1].value = temp;
+          setNumberArray(numberArray.map((obj, index) => array[index]));
         }
+        array[j].active = false;
+        array[j + 1].active = false;
+        setNumberArray(numberArray.map((obj, index) => array[index]));
       }
     }
   };
@@ -97,6 +111,19 @@ function SortingGraph() {
         <h1>
           {activeAlgorithm} -- {message}
         </h1>
+        <form>
+          <label>Speed: </label>
+          <select
+            onChange={handleSpeedChange}
+            disabled={message === "Sorting" ? true : false}
+          >
+            <option value={0}>Fastest</option>
+            <option value={20}>Fast</option>
+            <option value={50}>Mid</option>
+            <option value={80}>Slow</option>
+            <option value={150}>Slowest</option>
+          </select>
+        </form>
         <div className="header-buttons">
           <button
             onClick={handleNewArrayClick}
@@ -116,21 +143,39 @@ function SortingGraph() {
       </header>
 
       <div className="graph">
-        {numberArray.map((number) =>
-          message === "Sorted" ? (
-            <GraphBar
-              value={number}
-              color="lightgreen"
-              width={determineWidth(numberArray.length)}
-            />
-          ) : (
-            <GraphBar
-              value={number}
-              color="lightsalmon"
-              width={determineWidth(numberArray.length)}
-            />
-          )
-        )}
+        {numberArray.map((obj) => {
+          if (message === "Sorting") {
+            return obj.active === true ? (
+              <GraphBar
+                value={obj.value}
+                color="lightblue"
+                width={determineWidth(numberArray.length)}
+              />
+            ) : (
+              <GraphBar
+                value={obj.value}
+                color="salmon"
+                width={determineWidth(numberArray.length)}
+              />
+            );
+          } else if (message === "Sorted") {
+            return (
+              <GraphBar
+                value={obj.value}
+                color="lightgreen"
+                width={determineWidth(numberArray.length)}
+              />
+            );
+          } else {
+            return (
+              <GraphBar
+                value={obj.value}
+                color="salmon"
+                width={determineWidth(numberArray.length)}
+              />
+            );
+          }
+        })}
       </div>
     </div>
   );
